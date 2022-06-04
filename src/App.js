3d2152d141e2603d5c48bcef40fd28283f10d1e0
tamutamu00 <taikim080713@gmail.com>
2022-06-04 23:39:28 +0900

    errorを消す

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Header } from "./components/Header";
-import { Skills } from "./components/Skills";
+// import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
 
 function App() {
